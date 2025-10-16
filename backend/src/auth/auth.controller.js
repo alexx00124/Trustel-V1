6f291a76bf77b769f9dff342d8893774b7f9b394
alexx00134 <pinedaalexx00@gmail.com>
2025-10-15 20:50:08 -0500

    Add initial AuthController implementation and VSCode settings

--- backend/src/auth/auth.controller.js
+++ backend/src/auth/auth.controller.js
@@ -2,3 +2,6 @@ import { Controller } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {}
+
+
+//porque no se push en el otro computador tengo que terminar el auth controller tengo que presentalo en 15 dias y no he echo nada 
